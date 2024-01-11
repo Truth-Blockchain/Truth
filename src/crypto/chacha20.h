ff07d1fb62e0b94201b723565135ea94df177029
Truth-Blockchain <156115407+Truth-Blockchain@users.noreply.github.com>
2024-01-11 22:09:06 +0000

    Update chacha20.h
    Truthcore

--- src/crypto/chacha20.h
+++ src/crypto/chacha20.h
@@ -1,10 +1,11 @@
 // Copyright (c) 2017 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
+// Copyright (c) 2020-2024 The Truth Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef RAVEN_CRYPTO_CHACHA20_H
-#define RAVEN_CRYPTO_CHACHA20_H
+#ifndef TRUTH_CRYPTO_CHACHA20_H
+#define TRUTH_CRYPTO_CHACHA20_H
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -24,4 +25,4 @@ public:
     void Output(unsigned char* output, size_t bytes);
 };
 
-#endif // RAVEN_CRYPTO_CHACHA20_H
+#endif // TRUTH_CRYPTO_CHACHA20_H
