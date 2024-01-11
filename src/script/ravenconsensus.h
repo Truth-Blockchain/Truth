b0a88834ddd5aac23722082621ce73805d408ff8
Truth-Blockchain <156115407+Truth-Blockchain@users.noreply.github.com>
2024-01-11 23:54:31 +0000

    Update truthconsensus.h
    Truthcore

--- src/script/ravenconsensus.h
+++ src/script/ravenconsensus.h
@@ -1,16 +1,17 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
+// Copyright (c) 2020-2024 The Truth Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef RAVEN_RAVENCONSENSUS_H
-#define RAVEN_RAVENCONSENSUS_H
+#ifndef TRUTH_RAVENCONSENSUS_H
+#define TRUTH_RAVENCONSENSUS_H
 
 #include <stdint.h>
 
-#if defined(BUILD_RAVEN_INTERNAL) && defined(HAVE_CONFIG_H)
-#include "config/raven-config.h"
+#if defined(BUILD_TRUTH_INTERNAL) && defined(HAVE_CONFIG_H)
+#include "config/truth-config.h"
   #if defined(_WIN32)
     #if defined(DLL_EXPORT)
       #if defined(HAVE_FUNC_ATTRIBUTE_DLLEXPORT)
@@ -22,7 +23,7 @@
   #elif defined(HAVE_FUNC_ATTRIBUTE_VISIBILITY)
     #define EXPORT_SYMBOL __attribute__ ((visibility ("default")))
   #endif
-#elif defined(MSC_VER) && !defined(STATIC_LIBRAVENCONSENSUS)
+#elif defined(MSC_VER) && !defined(STATIC_LIBTRUTHCONSENSUS)
   #define EXPORT_SYMBOL __declspec(dllimport)
 #endif
 
@@ -34,46 +35,46 @@
 extern "C" {
 #endif
 
-#define RAVENCONSENSUS_API_VER 1
+#define TRUTHCONSENSUS_API_VER 1
 
-typedef enum ravenconsensus_error_t
+typedef enum truthconsensus_error_t
 {
-    ravenconsensus_ERR_OK = 0,
-    ravenconsensus_ERR_TX_INDEX,
-    ravenconsensus_ERR_TX_SIZE_MISMATCH,
-    ravenconsensus_ERR_TX_DESERIALIZE,
-    ravenconsensus_ERR_AMOUNT_REQUIRED,
-    ravenconsensus_ERR_INVALID_FLAGS,
-} ravenconsensus_error;
+    truthconsensus_ERR_OK = 0,
+    truthconsensus_ERR_TX_INDEX,
+    truthconsensus_ERR_TX_SIZE_MISMATCH,
+    truthconsensus_ERR_TX_DESERIALIZE,
+    truthconsensus_ERR_AMOUNT_REQUIRED,
+    truthconsensus_ERR_INVALID_FLAGS,
+} truthconsensus_error;
 
 /** Script verification flags */
 enum
 {
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_NONE                = 0,
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_P2SH                = (1U << 0), // evaluate P2SH (BIP16) subscripts
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 2), // enforce strict DER (BIP66) compliance
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY           = (1U << 4), // enforce NULLDUMMY (BIP147)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9), // enable CHECKLOCKTIMEVERIFY (BIP65)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10), // enable CHECKSEQUENCEVERIFY (BIP112)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 11), // enable WITNESS (BIP141)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_ALL                 = ravenconsensus_SCRIPT_FLAGS_VERIFY_P2SH | ravenconsensus_SCRIPT_FLAGS_VERIFY_DERSIG |
-                                                               ravenconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY | ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
-                                                               ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY | ravenconsensus_SCRIPT_FLAGS_VERIFY_WITNESS
+    truthconsensus_SCRIPT_FLAGS_VERIFY_NONE                = 0,
+    truthconsensus_SCRIPT_FLAGS_VERIFY_P2SH                = (1U << 0), // evaluate P2SH (BIP16) subscripts
+    truthconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 2), // enforce strict DER (BIP66) compliance
+    truthconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY           = (1U << 4), // enforce NULLDUMMY (BIP147)
+    truthconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9), // enable CHECKLOCKTIMEVERIFY (BIP65)
+    truthconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10), // enable CHECKSEQUENCEVERIFY (BIP112)
+    truthconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 11), // enable WITNESS (BIP141)
+    truthconsensus_SCRIPT_FLAGS_VERIFY_ALL                 = truthconsensus_SCRIPT_FLAGS_VERIFY_P2SH | truthconsensus_SCRIPT_FLAGS_VERIFY_DERSIG |
+                                                               truthconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY | truthconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
+                                                               truthconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY | truthconsensus_SCRIPT_FLAGS_VERIFY_WITNESS
 };
 
 /// Returns 1 if the input nIn of the serialized transaction pointed to by
 /// txTo correctly spends the scriptPubKey pointed to by scriptPubKey under
 /// the additional constraints specified by flags.
 /// If not nullptr, err will contain an error/success code for the operation
-EXPORT_SYMBOL int ravenconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
+EXPORT_SYMBOL int truthconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
                                                  const unsigned char *txTo        , unsigned int txToLen,
-                                                 unsigned int nIn, unsigned int flags, ravenconsensus_error* err);
+                                                 unsigned int nIn, unsigned int flags, truthconsensus_error* err);
 
-EXPORT_SYMBOL int ravenconsensus_verify_script_with_amount(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, int64_t amount,
+EXPORT_SYMBOL int truthconsensus_verify_script_with_amount(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, int64_t amount,
                                     const unsigned char *txTo        , unsigned int txToLen,
-                                    unsigned int nIn, unsigned int flags, ravenconsensus_error* err);
+                                    unsigned int nIn, unsigned int flags, truthconsensus_error* err);
 
-EXPORT_SYMBOL unsigned int ravenconsensus_version();
+EXPORT_SYMBOL unsigned int truthconsensus_version();
 
 #ifdef __cplusplus
 } // extern "C"
@@ -81,4 +82,4 @@ EXPORT_SYMBOL unsigned int ravenconsensus_version();
 
 #undef EXPORT_SYMBOL
 
-#endif // RAVEN_RAVENCONSENSUS_H
+#endif // TRUTH_TRUTHCONSENSUS_H
