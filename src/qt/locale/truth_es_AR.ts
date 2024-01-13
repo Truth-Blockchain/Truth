0aeaf3804f6295b6c191af3af36e47c27eb15e89
Truth-Blockchain <156115407+Truth-Blockchain@users.noreply.github.com>
2024-01-13 00:18:55 +0000

    Update truth_es_AR.ts
    Truthcore

--- src/qt/locale/truth_es_AR.ts
+++ src/qt/locale/truth_es_AR.ts
@@ -149,7 +149,7 @@
     <name>RPCConsole</name>
     </context>
 <context>
-    <name>RavenGUI</name>
+    <name>TruthGUI</name>
     </context>
 <context>
     <name>ReceiveCoinsDialog</name>
@@ -215,6 +215,6 @@
     <name>WalletView</name>
     </context>
 <context>
-    <name>raven-core</name>
+    <name>truth-core</name>
     </context>
 </TS>
